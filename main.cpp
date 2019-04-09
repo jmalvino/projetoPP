22e926676adbccdce567c2c7f2f270ae1f3cd338
JMALVINO <JMALVINO@DESKTOP-JMALVINO>
2019-04-09 19:31:35 -0300

    primeiro commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: JMALVINO
+ *
+ * Created on 9 de Abril de 2019, 19:28
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
