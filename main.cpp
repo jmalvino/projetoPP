dab02d69383b7503690a9090325733b15d549476
kamyr <kamyr@Ingrid>
2019-04-09 20:21:41 -0300

    #0002-Inicio questao01

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include<stdio.h>
 #include<omp.h>
 
-recebeFrase(){
-	char frase[200];
-  	printf("Digite a frase: ");
-	gets(frase);
-	printf("A frase armazenada foi: %s", frase);
-}
-questao01(char* frase[]){
-	int quantLetras[26];
-	#pragma omp parallel 
-	{
-		
-	}	
+recebeFrase() {
+    char frase[200];
+    printf("Digite a frase: ");
+    gets(frase);
+    printf("A frase armazenada foi: %s", frase);
 }
 
-int main(){
-	printf("%s",recebeFrase());
+questao01(char* frase[]) {
+    int quantLetras[26];
+#pragma omp parallel 
+    {
+#pragma omp for{
 
-	
+    }
+}
+}
+
+int main() {
+    printf("%s", recebeFrase());
+
+
 }
